26dc728dec5badf736b9f12b75678aaabee87ee8
Ramzy Kermad <kermad.r@sncf.fr>
2024-11-25 12:20:03 +0100

    Fixed Post DTO data class

--- app/src/main/java/com/example/instaclone/network/dto/posts_dto/PostDto.kt
+++ app/src/main/java/com/example/instaclone/network/dto/posts_dto/PostDto.kt
@@ -1,14 +1,20 @@
 package com.example.instaclone.network.dto.posts_dto
 
+import com.google.gson.annotations.SerializedName
+
 data class PostDto(
     val caption: String,
+    @SerializedName("comments")
     val commentDtos: List<CommentDto>?,
     val content: String,
     val likes: Int,
-    val post_id: String,
-    val profile_picture: String,
+    @SerializedName("post_id")
+    val postId: String,
+    @SerializedName("profile_picture")
+    val profilePicture: String,
     val shares: Int,
     val timestamp: String,
-    val user_id: String,
+    @SerializedName("user_id")
+    val userId: String,
     val username: String
 )
