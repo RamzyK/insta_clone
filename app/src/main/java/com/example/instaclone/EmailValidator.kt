e7384ef15b4c17c3ef903cfc20791fde84987b9b
Ramzy Kermad <kermad.r@sncf.fr>
2025-02-07 12:00:49 +0100

    Added email validator to test

---
+++ app/src/main/java/com/example/instaclone/EmailValidator.kt
@@ -0,0 +1,11 @@
+package com.example.instaclone
+
+class EmailValidator {
+
+    fun isValidEmail(email: String) : Boolean {
+        // Code the funciton to check if an email is valid
+
+        return false
+    }
+
+}
