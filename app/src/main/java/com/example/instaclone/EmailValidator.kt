16d456274362049b233794bed91a1142a56f8a4f
Ramzy Kermad <kermad.r@sncf.fr>
2025-02-07 16:14:34 +0100

    Init email validator class

---
+++ app/src/main/java/com/example/instaclone/EmailValidator.kt
@@ -0,0 +1,10 @@
+package com.example.instaclone
+
+class EmailValidator {
+
+    fun isValidEmail(email: String) : Boolean {
+        // Ecrire les vérifications liées à des règles métier
+        
+        return true
+    }
+}
